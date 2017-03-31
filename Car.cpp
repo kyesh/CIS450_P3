b8e61ad42cb2b2ae9f5de90fcfff7aa91c8ca801
user <kenyesh@gmail.com>
2017-03-31 13:12:30 -0400

    It Compiles!

--- Car.cpp
+++ Car.cpp
@@ -14,30 +14,35 @@ class Car
 int id; // id
 double inter_arrival_t; // inter-arrival time between this vehicle and next
 int dir; // heading direction
+public:
+	Car(int, double, int);
+	void ArriveBridge();
+	void CrossBridge();
+	void ExitBridge();
 
 }; // end Car class
 
 // start Car Constructor
 
-	Car::Car(int carid, double arrival_time, int direction)
-	{
-		id = carid;
-		inter_arrival_t = arrival_time
-		dir = direction;
-	}
+Car::Car(int carid, double arrival_time, int direction)
+{
+	id = carid;
+	inter_arrival_t = arrival_time;
+	dir = direction;
+}
 
 
-	void Car::ArriveBridge()
-	{
-	
-	}
+void Car::ArriveBridge()
+{
 
-	void Car::CrossBridge()
-	{
-	
-	}
+}
 
-	void Car::ExitBridge()
-	{
-	
-	}
+void Car::CrossBridge()
+{
+
+}
+
+void Car::ExitBridge()
+{
+
+}
