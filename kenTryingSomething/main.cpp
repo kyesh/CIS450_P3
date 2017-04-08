0fa083e6f110b049380161abb3f6ece824269b94
user <kenyesh@gmail.com>
2017-04-08 00:21:29 -0400

    Tired going to bed

--- kenTryingSomething/main.cpp
+++ kenTryingSomething/main.cpp
@@ -1,5 +1,12 @@
-//Reference for sleeping http://stackoverflow.com/questions/4184468/sleep-for-milliseconds
 
+// From Prof example file
+#include <stdio.h>
+#include <pthread.h>
+#include <assert.h>
+#include <stdlib.h>
+
+
+//Reference for sleeping http://stackoverflow.com/questions/4184468/sleep-for-milliseconds
 #include <chrono>
 #include <thread>
 
@@ -7,18 +14,23 @@
 #define GOING   1
 #define DONE    2
 
+//Array of Structures http://fresh2refresh.com/c-programming/c-array-of-structures/
+
 typedef struct _vehicle_info {
    int id; // id
    char dir; // heading direction
    double inter_arrival_t; // inter-arrival time between this vehicle and next
 } vehicle_info;
 
+int GlobaGoNum = -1;
+vehicle_info vQue[N];
+int status sQue[N];
 
-void* VehicleAction(void* arg) {
-   vehicle_info *ti = (vehicle_info *) arg;
-   ArriveBridge(ti);
-   CrossBridge(ti);
-   ExitBridge(ti);
+changeStatusTo(ti, int status){
+
+   sQue[it.id] = status;
+   updateGoNum();
+
 }
 
 void ArriveBridge(vechicle_info ti){
@@ -44,5 +56,29 @@ void ExitBridge(vechicle_info ti){
 
    changeStatusTo(ti, DONE);
 
+}
+
+
+void* VehicleAction(void* arg) {
+   vehicle_info *ti = (vehicle_info *) arg;
+   ArriveBridge(ti);
+   CrossBridge(ti);
+   ExitBridge(ti);
+}
+
+int main(int argc, char *argv[]) {
+//Array of pthread_t http://stackoverflow.com/questions/14155055/how-to-save-pthread-t-id-to-an-array
+
+	pthread_t p[N];
+
+   for (int i = 0; i < N; i++) {
+       std::this_thread::sleep_for(std::chrono::milliseconds(1000*p[i].inter_arrival_t));
+       pthread_create(&p[i], NULL, VehicleAction, &vQue[i]);
+   }
+
+
+	pthread_join(p, (void **) &m);
+
+	return 0;
 }
 
